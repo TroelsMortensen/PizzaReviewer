e4ba301bec28a6239de344baccfeef7fbdc958e5
TRMO <tromortensen@hotmail.com>
2023-01-09 21:07:02 +0100

    Remove unneeded try catch.

--- UI/UI/src/main/java/consoleui/ConsoleUI.java
+++ UI/UI/src/main/java/consoleui/ConsoleUI.java
@@ -2,8 +2,7 @@ package consoleui;
 
 import jsondata.DataAccess;
 
-public class ConsoleUI
-{
+public class ConsoleUI {
 
     private final DataAccess dataAccess;
 
@@ -11,12 +10,8 @@ public class ConsoleUI
         this.dataAccess = dataAccess;
     }
 
-    public void run(){
-        try {
-            System.out.println("Running super fast, going nowhere.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void run() {
+        System.out.println("Running super fast, going nowhere.");
     }
 
 }
