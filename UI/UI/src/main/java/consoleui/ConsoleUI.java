a5ddb301352698db6aa9650d80da14bb00094408
TRMO <tromortensen@hotmail.com>
2023-01-09 20:34:01 +0100

    Bad commit message in one go

---
+++ UI/UI/src/main/java/consoleui/ConsoleUI.java
@@ -0,0 +1,17 @@
+package consoleui;
+
+import jsondata.DataAccess;
+
+public class ConsoleUI
+{
+
+    private final DataAccess dataAccess;
+
+    public ConsoleUI(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void run(){
+        System.out.println("Running super fast, going nowhere.");
+    }
+}
---
+++ src/main/java/startup/StartApp.java
@@ -0,0 +1,15 @@
+package startup;
+
+import consoleui.ConsoleUI;
+import jsondata.DataAccess;
+import jsondata.JsonDataAccess;
+
+public class StartApp {
+
+    public static void main(String[] args) {
+        DataAccess dataAccess = new JsonDataAccess();
+        ConsoleUI ui = new ConsoleUI(dataAccess);
+        ui.run();
+    }
+
+}
