5967ef26636d5113114d301c55f8105b1954336c
Troels Mortensen <tromortensen@hotmail.com>
2023-01-10 15:13:33 +0100

    Display two more options in the main menu

--- UI/UI/src/main/java/consoleui/MainMenuHandler.java
+++ UI/UI/src/main/java/consoleui/MainMenuHandler.java
@@ -5,6 +5,8 @@ public class MainMenuHandler {
   public void showMainMenu() {
      while(true) {
       System.out.println("Main menu:");
+      System.out.println("1) Add review"); 
+      System.out.println("2) Update review");
     } 
   }
 }
