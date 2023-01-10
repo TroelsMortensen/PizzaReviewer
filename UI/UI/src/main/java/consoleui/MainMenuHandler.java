9624ad1433148ce59c8133a9df2e7f91fc7631ac
Troels Mortensen <tromortensen@hotmail.com>
2023-01-10 15:18:14 +0100

    Add options for delete/see reviews to main menu

--- UI/UI/src/main/java/consoleui/MainMenuHandler.java
+++ UI/UI/src/main/java/consoleui/MainMenuHandler.java
@@ -7,6 +7,8 @@ public class MainMenuHandler {
       System.out.println("Main menu:");
       System.out.println("1) Add review"); 
       System.out.println("2) Update review");
+      System.out.println("3) Delete review");
+      System.out.println("4) See reviews");
     } 
   }
 }
