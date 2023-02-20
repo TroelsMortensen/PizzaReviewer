95254b6dde9f5950d34a55b992ad0f3bc90ec670
TRMO <tromortensen@hotmail.com>
2023-02-20 12:12:01 +0100

    change option 3 naming

--- UI/UI/src/main/java/consoleui/MainMenuHandler.java
+++ UI/UI/src/main/java/consoleui/MainMenuHandler.java
@@ -60,7 +60,7 @@ public class MainMenuHandler {
         System.out.println("Main menu:");
         System.out.println("1) Add review");
         System.out.println("2) Update review");
-        System.out.println("3) Delete review");
+        System.out.println("3) Remove review");
         System.out.println("4) See reviews");
         System.out.println("<) Exit");
     }
